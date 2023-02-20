660af34513308765b841254e9888471a65d5f6c1
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-02-20 14:04:42 -0600

    feat: Adding app functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useUser } from './hooks';
 import './App.css';
+import UserSearch from './modules/UserSearch/UserSearch';
+import UserCard from './modules/UserCard/UserCard';
+import SkeletonLoader from './modules/Loader/Loader';
 
 function App() {
+  const [search, setSearch] = useState('migigim')
+  const { loading, userData } = useUser(search)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <UserSearch setValue={setSearch} />
+      {
+        loading ? <SkeletonLoader /> : <UserCard userData={userData} />
+      }
+    </div >
   );
 }
 
